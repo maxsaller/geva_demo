e0d9d707c1a00cb69045cb2456e612db0ddf6e21
Max Saller <maximilian.a.c.saller@gmail.com>
2023-07-17 11:29:56 -0400

    feat: adds markdown file for talk outline and links

--- multiprocessing/mp_pool.py
+++ multiprocessing/mp_pool.py
@@ -9,29 +9,39 @@ import subprocess as sp
 import multiprocessing as mp
 import matplotlib.pyplot as plt
 
+
 def fac(n: int) -> int:
+    """Return factorial."""
     fac = 1
     for i in range(n, 1, -1):
         fac *= i
     return fac
 
+
 def Mq(q: int) -> int:
+    """Factorial term in Chudnovsky."""
     return fac(6*q)//(fac(3*q)*fac(q)**3)
 
+
 def Lq(q: int) -> int:
+    """Linear term in Chudnovsky."""
     return 545140134 * q + 13591409
 
+
 def Xq(q: int) -> int:
+    """Exponential term in Chudnovsky."""
     return int(-262537412640768000)**q
 
+
 def piChudnovsky(n: int) -> None:
-    """Chudnovsky algorith for computing pi."""
+    """Chudnovsky algorithm for computing pi."""
     print(f"Chudnovsky with n={n}")
     C = 426880 * np.sqrt(10005)
     pi = 0
     for i in range(n):
         pi += (Mq(i)*Lq(i)/Xq(i))
     print(f"Chudnovsky with n={n}... Done! pi={C/pi}")
+
 
 if __name__ == "__main__":
 
@@ -43,38 +53,40 @@ if __name__ == "__main__":
             "-e 'btop' " +
             ">/dev/null 2>/dev/null &"],
            shell=True, check=False, capture_output=True)
-    time.sleep(10)
+    time.sleep(5)
 
     # Example with computation in Python
     print("Python example: Chudnovsky algorithm for computing pi")
     with mp.Pool(processes=8) as p:
-        p.map(piChudnovsky, [i*10 for i in range(15,31)])
+        p.map(piChudnovsky, [i*100 for i in range(10, 25)])
         p.close()
         p.join()
     print("\n\n")
 
     # Example with an external executable
     def runFortran(run: int) -> None:
+        """Set up and run embarrassingly parallel FORTRAN code."""
         os.mkdir(f"runs/run{run:0>2}")
         shutil.copy("fmap/fmap.x", f"runs/run{run:0>2}/")
         shutil.copy("fmap/input", f"runs/run{run:0>2}/")
         print(f"Running fmap.x in runs/run{run:0>2}... ")
+
         sp.run([f"./fmap.x > run{run:0>2}.log"],
                shell=True, cwd=f"runs/run{run:0>2}")
-        print(f"Running fmap.x in runs/run{run:0>2}... Done")
-    
-    print("Executable example: Semiclassical dynamics with FORTRAN")
-    # os.mkdir("runs")
-    # with mp.Pool(processes=8) as p:
-    #     p.map(runFortran, [i+1 for i in range(16)])
-    #     p.close()
-    #     p.join()
-    # dat = np.genfromtxt("runs/run01/Cpop.out")
-    # for i in range(1,16):
-    #     dat += np.genfromtxt(f"runs/run{i+1:0>2}/Cpop.out")
-    # dat /= 16
 
-    # plt.plot(dat[1:, 0], dat[1:, 1])
-    # plt.plot(dat[1:, 0], dat[1:, 2])
-    # plt.show()
-    
+        print(f"Running fmap.x in runs/run{run:0>2}... Done")
+
+    print("Executable example: Semiclassical dynamics with FORTRAN")
+    os.mkdir("runs")
+    with mp.Pool(processes=8) as p:
+        p.map(runFortran, [i+1 for i in range(16)])
+        p.close()
+        p.join()
+    dat = np.genfromtxt("runs/run01/Cpop.out")
+    for i in range(1, 16):
+        dat += np.genfromtxt(f"runs/run{i+1:0>2}/Cpop.out")
+    dat = dat / 16
+
+    plt.plot(dat[1:, 0], dat[1:, 1])
+    plt.plot(dat[1:, 0], dat[1:, 2])
+    plt.show()
---
+++ talk.md
@@ -0,0 +1,39 @@
+
+# Group Talk 7/17/23
+- [x] A few notes on Geva Cluster usage
+	- [x] Prioritize using your <u>own</u> node at 100% uptime/load
+	- [x] Expand to other nodes only when that is not sufficient
+<br>
+- [x] Python `multiprocessing` Pools
+	- [x] Monitoring CPU usage with `suprocessing` call to `btop`
+	- [x] Example with Chudnovsky algorithm for computing $\pi$
+	- [x] Example with `fmap` FORTRAN example
+<br>
+- [x] HTML & CSS demonstration
+	- [x] The magic of `index.html`
+	- [x] The #1 rule of (amateur) web design:<br><br><span style="font-family: Z003; font-size: 150%; color: red; border: 1pt solid; padding: .3em;">Don't do it yourself! Use a template!</span><br>
+	- [x] CSS a.k.a. making things look not terrible: [[#CSS Example]]
+	- [x] [maxsaller.github.io](https://maxsaller.github.io)
+	- [x] [HTML5 UP](https://html5up.net/)
+<br><br><br>
+
+### CSS Example
+
+HTML uses tags to create the structure of a page:
+```html
+<span class="rule">
+	Don't do it yourself! Use a template!
+</span>
+```
+
+
+While CSS is used to style the appearance and behavior of tags:
+```CSS
+.rule {
+	font-family: Z003;
+	font-size: 150%;
+	color: red;
+	border: 1pt solid;
+	padding: .3em;
+}
+```
